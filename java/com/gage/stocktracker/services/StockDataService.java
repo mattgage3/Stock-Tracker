cd9b5e233bbdc10148587bef736a3eeb3747b4ea
Matthew Gage <62771600+mattgage3@users.noreply.github.com>
2020-06-01 18:22:00 -0400

    Update StockDataService.java

--- java/com/gage/stocktracker/services/StockDataService.java
+++ java/com/gage/stocktracker/services/StockDataService.java
@@ -12,19 +12,26 @@ public class StockDataService {
 	public static String companyTicker;
 	public static String tradeDay;
 	public static double openPrice;
-	public static double closePrice;
+	public static double latestPrice;
 	public static double highPrice;
 	public static double lowPrice;
 	
 	public static void fetchStockData(String stock) throws IOException, InterruptedException {
 		
-		// fetch JSON data via HTTP request
+		// fetch name, symbol, latest price JSON data
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request =HttpRequest.newBuilder()
 				.uri(URI.create("https://cloud.iexapis.com/stable/stock/" + stock + "/book?token=pk_7d16af48d942497a92a72c3afa0630b5"))
 				.build();
 		HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
 		
+		// fetch previous price  JSON data 
+		HttpClient priceClient = HttpClient.newHttpClient();
+		HttpRequest priceRequest = HttpRequest.newBuilder()
+						.uri(URI.create("https://sandbox.iexapis.com/stable/stock/" + stock + "/previous?token=Tsk_0cee79b628fd4dbe88ecb38a7394c659"))
+						.build();
+		HttpResponse<String> httpPriceResponse = priceClient.send(priceRequest, HttpResponse.BodyHandlers.ofString());
+				
 		
 		// parse JSON data
 		try {
@@ -32,11 +39,13 @@ public class StockDataService {
 			companyName = object.getJSONObject("quote").getString("companyName");
 			companyTicker = object.getJSONObject("quote").getString("symbol");
 			tradeDay = object.getJSONObject("quote").getString("latestTime");
-			openPrice = object.getJSONObject("quote").getDouble("open");
-			closePrice = object.getJSONObject("quote").getDouble("close");
-			highPrice = object.getJSONObject("quote").getDouble("high");
-			lowPrice = object.getJSONObject("quote").getDouble("low");
-
+			latestPrice = object.getJSONObject("quote").getDouble("latestPrice");
+			
+			JSONObject priceObject = new JSONObject(httpPriceResponse.body());
+			openPrice = priceObject.getDouble("open");
+			highPrice = priceObject.getDouble("high");
+			lowPrice = priceObject.getDouble("low");
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
